39aa71c51855f439356ccfcaf01fc064ef8da710
Natanaelel <43886350+Natanaelel@users.noreply.github.com>
2021-05-25 10:06:19 +0200

    Delete addon directory

--- addon/number.js
+++
@@ -1,3 +0,0 @@
-Object.defineProperty(Number.prototype,"chr",{get:function(){return(String.fromCharCode(this))}})
-Object.defineProperty(Number.prototype,"to_s",{get:function(){return(this.toString())}})
-Object.defineProperty(Number.prototype,"digits",{get:function(){return(this.toString().match(/\d/g).map(c=>parseInt(c)).reverse())}})
--- addon/string.js
+++
@@ -1,7 +0,0 @@
-Object.defineProperty(String.prototype,"sum",{get:function(){return(this.split("").map(c=>c.charCodeAt()).reduce((a,b)=>a+b,0))}})
-Object.defineProperty(String.prototype,"chars",{get:function(){return(this.split(""))}})
-Object.defineProperty(String.prototype,"ord",{get:function(){return(this.charCodeAt())}})
-Object.defineProperty(String.prototype,"to_i",{get:function(){return(parseInt(this))}})
-Object.defineProperty(String.prototype,"reverse",{get:function(){return(this.split("").reverse().join(""))}})
-String.prototype.tr = function(rep, sub){return(this.split("").map(c=>rep.includes(c)?sub[Math.min(rep.indexOf(c),sub.length-1)]:c).join(""))}
-
