c72d03eb65c9609dc1576e895d470f901f2e2758
Natanaelel <43886350+Natanaelel@users.noreply.github.com>
2021-06-12 19:45:46 +0200

    Update number.js

--- number.js
+++ number.js
@@ -3,9 +3,11 @@ Object.defineProperty(Number.prototype,"s",{get:function(){return(this.toString(
 Object.defineProperty(Number.prototype,"digits",{get:function(){return(this.toString().match(/\d/g).map(c=>parseInt(c)).reverse())}}) // array of digits
 
 Object.defineProperty(Number.prototype,"abs",{get:function(){return(Math.abs(this))}}) // absolute value
+Object.defineProperty(Number.prototype,"sqrt",{get:function(){return(Math.sqrt(this))}}) // absolute value
 
 Object.defineProperty(Number.prototype,"fl",{get:function(){return(Math.floor(this))}}) // floor
 Object.defineProperty(Number.prototype,"ce",{get:function(){return(Math.ceil(this))}}) // ceil
+Object.defineProperty(Number.prototype,"rd",{get:function(){return(Math.round(this))}}) // round
 Object.defineProperty(Number.prototype,"i",{get:function(){return(parseInt(this))}}) // int
 
 Object.defineProperty(Number.prototype,"bin",{get:function(){return(this.toString(2))}}) // binary string
@@ -17,10 +19,25 @@ Object.defineProperty(Number.prototype,"prime",{
     get:function(){
         if(this <= 1){return(false)}
         if(this == 2){return(true)}
-        for(let i = Math.ceil(Math.sqrt(this)); i >1; i--){
+        for(let i = Math.ceil(Math.sqrt(this)); i > 1; i--){
             if(this%i==0)return(false)
         }
         return(true)
     }
 }) // check if number is prime
 
+
+
+
+
+Number.prototype.log = function(){
+    console.log(this.valueOf())
+}
+
+Number.prototype.debug = function(func){
+    if(func){
+        console.log(func(this))
+    }else{
+        console.log(this)
+    }
+}
