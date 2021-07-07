c14615ff6be778aeaff45e3213393616c4a0b1c2
Natanaelel <43886350+Natanaelel@users.noreply.github.com>
2021-07-07 20:15:41 +0200

    Update array.js

--- array.js
+++ array.js
@@ -108,7 +108,7 @@ Array.prototype.shuffle = function(){
   for (let i = this.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     const temp = this[i];
-    this[i] = array[j];
+    this[i] = this[j];
     this[j] = temp;
   }
 }
